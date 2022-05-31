c25fa7da4371c3fa4a8ae18eb405fb8d3a31e101
Razvan <costin.pahontu@gmail.com>
2022-05-31 11:12:08 +0300

    bat

--- samples/small-company/execute-java-pdf.bat
+++ samples/small-company/execute-java-pdf.bat
@@ -1,1 +1,10 @@
-java  -Duser=supplier@gmail.com -Dpassword=1234 -Dtype=pdf -Dpdf-input=./docs/model.pdf -Dpdf-xml-input=./docs/msg.xml -Dpdf-client.signature=./docs/semnatura.jpeg -Dpdf-supplier.signature=./docs/semnatura.jpeg -Dpdf-operator.signature=./docs/semnatura.jpeg -jar posf-1.jar
+java  ^
+-Duser=supplier@gmail.com ^
+-Dpassword=1234 ^
+-Dtype=pdf ^
+-Dpdf-input=./docs/model.pdf ^
+-Dpdf-xml-input=./docs/msg.xml ^
+-Dpdf-client.signature=./docs/semnatura.jpeg ^
+-Dpdf-supplier.signature=./docs/semnatura.jpeg ^
+-Dpdf-operator.signature=./docs/semnatura.jpeg ^
+-jar posf-1.jar
