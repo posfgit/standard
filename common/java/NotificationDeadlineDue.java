22d0fc6c7a82e2903e5ebc66ce984afac5adc337
Razvan Pahontu <costin.pahontu@gmail.com>
2022-11-08 12:37:01 +0200

    update java

---
+++ common/java/NotificationDeadlineDue.java
@@ -0,0 +1,126 @@
+
+package ro.anre.posf.standard;
+
+import java.time.LocalDate;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+
+/**
+ * Se emite din POSF automat pentru toate contractele care vor ajunge la termen in 7 zile calendaristice
+ * 
+ * <p>Java class for NotificationDeadlineDue complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="NotificationDeadlineDue">
+ *   &lt;complexContent>
+ *     &lt;extension base="{http://www.anre.ro/ANRESchema}Message">
+ *       &lt;sequence>
+ *         &lt;element name="contract" type="{http://www.anre.ro/ANRESchema}Contract"/>
+ *         &lt;element name="info" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *         &lt;element name="dueDate" type="{http://www.w3.org/2001/XMLSchema}date"/>
+ *       &lt;/sequence>
+ *     &lt;/extension>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "NotificationDeadlineDue", propOrder = {
+    "contract",
+    "info",
+    "dueDate"
+})
+public class NotificationDeadlineDue
+    extends Message
+{
+
+    @XmlElement(required = true)
+    protected Contract contract;
+    protected String info;
+    @XmlElement(required = true, type = String.class)
+    @XmlJavaTypeAdapter(Adapter2 .class)
+    @XmlSchemaType(name = "date")
+    protected LocalDate dueDate;
+
+    /**
+     * Gets the value of the contract property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Contract }
+     *     
+     */
+    public Contract getContract() {
+        return contract;
+    }
+
+    /**
+     * Sets the value of the contract property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Contract }
+     *     
+     */
+    public void setContract(Contract value) {
+        this.contract = value;
+    }
+
+    /**
+     * Gets the value of the info property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getInfo() {
+        return info;
+    }
+
+    /**
+     * Sets the value of the info property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setInfo(String value) {
+        this.info = value;
+    }
+
+    /**
+     * Gets the value of the dueDate property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public LocalDate getDueDate() {
+        return dueDate;
+    }
+
+    /**
+     * Sets the value of the dueDate property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setDueDate(LocalDate value) {
+        this.dueDate = value;
+    }
+
+}
